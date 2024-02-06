db4293dcd449f0009275e793f343a3f2af4f6971
Khaled Hossain Shihab <khaledhossain778@gmail.com>
2024-02-06 21:28:52 +0600

    project initiation

--- resources/views/assets/css.blade.php
+++ resources/views/assets/css.blade.php
@@ -1,8 +1,24 @@
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="style.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500&family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
+<link
+    href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500&family=Lato:wght@300;400;700&display=swap"
+    rel="stylesheet">
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap');
+
+    * {
+        font-family: 'Inter', sans-serif;
+    }
+
+    .hover-btn:hover {
+        background: #7CB4CC;
+        border: 1px solid #7CB4CC;
+        transition: 0.2s ease-in;
+    }
+</style>
 
 <script src="js/modernizr.js"></script>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,33 +1,34 @@
 <!DOCTYPE html>
-    <html lang="en">
-        <head>
-            {{--
-            <meta charset="utf-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta name="format-detection" content="telephone=no">
-            <meta name="apple-mobile-web-app-capable" content="yes">
-            <meta name="author" content="">
-            <meta name="keywords" content="">
-            <meta name="description" content="">
-            --}}
-            <title>Ministore</title>
+<html lang="en">
+<head>
+    {{--
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="author" content="">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    --}}
+    <title>Ministore</title>
 
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-            @include('assets.css')
-        </head>
+    @include('assets.css')
+</head>
 
-        <body data-bs-spy="scroll" data-bs-target="#navbar" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" tabindex="0">
+<body data-bs-spy="scroll" data-bs-target="#navbar" data-bs-root-margin="0px 0px -40%"
+      data-bs-smooth-scroll="true" tabindex="0">
 
-            @include('header')
+@include('header')
 
-            @yield('content')
+@yield('content')
 
-            @include('footer')
+@include('footer')
 
-            @include('assets.script')
+@include('assets.script')
 
-        </body>
-    </html>
+</body>
+</html>
