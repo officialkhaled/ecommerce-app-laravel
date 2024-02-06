cb83f3a8a9dc110f4d00ccc07eee1f3996441e84
Khaled <khaled@skylarksoft.com>
2024-02-06 13:54:10 +0600

    project initiation

---
+++ resources/views/assets/script.blade.php
@@ -0,0 +1,5 @@
+<script src="js/jquery-1.11.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" src="js/plugins.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
